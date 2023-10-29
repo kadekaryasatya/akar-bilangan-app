0bddc7cb097afdeb16e997b4ee4a4de22a22f033
Dek Arya <arya.satyad123@gmail.com>
2023-10-30 01:14:08 +0800

    update UI

--- src/components/AkarBilanganCalculator.jsx
+++ src/components/AkarBilanganCalculator.jsx
@@ -10,6 +10,12 @@ const AkarBilanganCalculator = () => {
   const [waktuPemrosesan, setWaktuPemrosesan] = useState("");
   const [data, setData] = useState([]);
   const [validasiError, setValidasiError] = useState(false);
+  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(data.length / itemsPerPage);
 
   useEffect(() => {
     fetchData();
@@ -49,7 +55,6 @@ const AkarBilanganCalculator = () => {
       setWaktuPemrosesan(response.data.waktu_pemrosesan);
       setBilanganInput(bilangan);
       setValidasiError(false);
-      setBilangan("");
       // Setelah berhasil menyimpan data ke database, tambahkan data baru ke state
       setData([...data, response.data]);
       toast.success("Successful input data", {
@@ -65,7 +70,6 @@ const AkarBilanganCalculator = () => {
           position: toast.POSITION.TOP_RIGHT,
           autoClose: 5000,
         });
-        setBilangan("");
         setValidasiError(true);
       } else {
         // Handle other errors
@@ -78,19 +82,31 @@ const AkarBilanganCalculator = () => {
     }
   };
 
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
-    <div className="my-8 lg:mx-28 mx-10 min-h-screen ">
+    <div className="lg:my-8 lg:mx-28  min-h-screen mx-5 my-5">
       <ToastContainer />
       <div className="shadow-lg p-10 border  ">
-        <h1 className="text-3xl font-semibold mb-10 text-blue-900">
-          Kalkulator Akar Bilangan
-        </h1>
+        <div className="flex justify-between items-center mb-10">
+          <h1 className="lg:text-3xl text-xl font-semibold text-blue-900">
+            Kalkulator Akar Bilangan
+          </h1>
+          <button
+            className="px-4 py-2 bg-blue-100 text-white rounded hover:bg-blue-200"
+            onClick={handleRefresh} // Tambahkan handler yang sesuai
+          >
+            <img src="/refresh.png" alt="Refresh" className="w-[20px]" />
+          </button>
+        </div>
         <div className="">
-          <div className="flex gap-4 justify-between items-center">
+          <div className="lg:flex gap-4 justify-between items-center">
             <input
               placeholder="Masukkan bilangan"
               type="text"
-              className={` border-[2px] rounded px-3 py-2 w-[400px] ${
+              className={` border-[2px] rounded px-3 py-2 lg:w-1/2 w-full ${
                 validasiError && "border-red-500"
               }`}
               value={bilangan}
@@ -102,7 +118,7 @@ const AkarBilanganCalculator = () => {
               }}
             />
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full lg:w-[200px] mt-5 lg:mt-0"
               onClick={handleCalculate}
             >
               Hitung Akar
@@ -123,36 +139,59 @@ const AkarBilanganCalculator = () => {
         )}
 
         {data.length > 0 ? (
-          <div className="mt-10 ">
+          <div className="mt-10  ">
             <div className=" ">
               <table
                 className="text-sm text-left text-gray-500 dark:text-gray-400 "
-                style={{ width: "100%" }}
+                style={{ width: "100%", overflowX: "auto" }}
               >
-                <thead className="text-xs  uppercase bg-gray-50 dark:bg-blue-900 text-white">
+                <thead className="text-xs  uppercase bg-gray-50 dark:bg-blue-900 text-white ">
                   <tr>
-                    <th className="px-4 py-2 border">Bilangan</th>
-                    <th className="px-4 py-2 border ">Hasil Akar</th>
-                    <th className="px-4 py-2 border">
+                    <th className="lg:px-4 lg:py-2 px-1 py-1 border">
+                      Bilangan
+                    </th>
+                    <th className="lg:px-4 lg:py-2 px-1 py-1 border ">
+                      Hasil Akar
+                    </th>
+                    <th className="lg:px-4 lg:py-2 px-1 py-1border">
                       Waktu Pemrosesan (detik)
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  {data.map((item) => (
+                  {currentItems.map((item) => (
                     <tr
                       key={item.id}
-                      className=" border-b border-gray-800  text-gray-800"
+                      className=" border-b border-gray-800  text-gray-800 text-xs lg:text-sm"
                     >
-                      <td className="px-4 py-2 border">{item.bilangan}</td>
-                      <td className="px-4 py-2 border">{item.akar}</td>
-                      <td className="px-4 py-2 border">
+                      <td className="lg:px-4 lg:py-2 px-1 py-1 border">
+                        {item.bilangan}
+                      </td>
+                      <td className="lg:px-4 lg:py-2 px-1 py-1 border">
+                        {item.akar}
+                      </td>
+                      <td className="lg:px-4 lg:py-2 px-1 py-1 border">
                         {item.waktu_pemrosesan}
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
+              <div className=" mt-4 ">
+                {Array.from({ length: totalPages }, (v, i) => (
+                  <button
+                    key={i}
+                    onClick={() => setCurrentPage(i + 1)}
+                    className={`px-2 py-1 mx-[1px] text-xs lg:text-base ${
+                      currentPage === i + 1
+                        ? "bg-blue-500 text-white"
+                        : "bg-gray-300 text-gray-600"
+                    } rounded`}
+                  >
+                    {i + 1}
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
         ) : (
